19f0a38af68316134bdabd4900790fffb1e78441
Vermanubis <vermanubis1@gmail.com>
2020-09-08 18:47:23 -0600

    update

--- VermanubisSite/settings.py
+++ VermanubisSite/settings.py
@@ -119,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'butterg@eou.edu'
+SERVER_EMAIL = 'vermanubis1@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vermanubis1@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sarc0phagus!'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 
 def mainpage(request):
     context = {'lol' : 'lol'}
     return render(request, 'mainpage/mainpage.html', context)
+    
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+      
+        msg = EmailMessage(message_name, message, message_email, to=['vermanubis1@gmail.com'])
+        msg.send()
+        
+        return render(request, 'mainpage/contact.html', {'message_name' : message_name})
+        
+    else:
+        return render(request, 'mainpage/contact.html', {})
