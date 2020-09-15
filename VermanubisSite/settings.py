892caa4a2f13e9a5ea36a3c7e1fcdede22173777
Vermanubis <vermanubis1@gmail.com>
2020-09-14 22:35:44 -0600

    deploy attempt

--- VermanubisSite/settings.py
+++ VermanubisSite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'f(tf0qth(-6_oy9b0%6)i-%#f3jspafg4srdz81*!$h%b_*=*x' #os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
